8f5c61d243fdf0ec36f17cf049096890fed82e34
ryland <ryland@MacBook-Pro-4.hawaii.rr.com>
2024-04-05 19:46:01 -1000

    closer to finishing auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,15 +10,35 @@ router.get('/', (req, res)=>{
     res.send('Hello from auth route!')
 })
 
-router.post('/login', (req, res)=>{
-
+router.post('/login', async (req, res)=>{
+    const userToLogin = await User.findOne({username: req.body.username})
+    if (userToLogin){
+        bcrypt.compare(req.body.password, userToLogin.password, (err, result)=>{
+            if (result){
+                const token = jwt.sign({username: req.body.username}, 'keyboard', {expiresIn: '1h'});
+                res.cookie('token', token, {httpOnly: true}).json({Status: 'Success'})
+            } else if (err) {
+                res.status(400).json({Status: 'Error', message: 'incorrect username or password'})
+            } else {
+                res.status(400).json({Status: 'Error', message: 'incorrect username or password'})
+            }
+        })
+    } else {
+        res.status(400).json({Status: 'Error', message: 'incorrect username or password'})
+    }
 })
 
 router.post('/signup', async (req, res)=>{
-    bcrypt.hash(req.body.password, saltRounds, async (err, hashedPassword)=>{
-        const createdUser = await User.create({...req.body, password: hashedPassword})
-        res.json(createdUser)
-    })
+    console.log(req.body.username, req.body.password)
+    const existingUser = await User.findOne({username: req.body.username})
+    if (existingUser){
+        res.status(400).json({Status: 'Error', message: 'exists'})
+    } else {
+        bcrypt.hash(req.body.password, saltRounds, async (err, hashedPassword)=>{
+            const createdUser = await User.create({...req.body, password: hashedPassword})
+            res.json(createdUser)
+        })
+    }
 })
 
 router.get('/logout', (req, res)=>{
--- server.js
+++ server.js
@@ -10,11 +10,18 @@ const PORT = process.env.PORT
 const secret = process.env.SECRET
 const jwt = require('jsonwebtoken')
 const authController = require('./controllers/authController')
+const cookieParser = require('cookie-parser')
 
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+
+app.use(cors(corsOptions))
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.get('/', (req, res)=>{
     res.send('Hello World')
